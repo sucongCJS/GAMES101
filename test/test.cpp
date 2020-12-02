7a5a6bc46270dcd43ddc74e33282b10eb4af6b21
Sucong <sucongjs@outlook.com>
2020-12-02 11:59:41 +0800

    env configure

--- test/test.cpp
+++ test/test.cpp
@@ -1,18 +1,16 @@
-#include<opencv2/opencv.hpp>
-#include<opencv2/highgui.hpp>
-#include<eigen3/Eigen/Eigen>
-#include<opencv2/opencv.hpp>
+//#include<opencv2/opencv.hpp>
+//#include<opencv2/highgui.hpp>
+//#include<eigen3/Eigen/Eigen>
+//#include<opencv2/opencv.hpp>
 #include<iostream>
-#include <string>
-#include <functional>
-#include <optional>
-#include <fstream>
-using namespace cv;
-using namespace Eigen;
+//#include <string>
+//#include <functional>
+//#include <optional>
+//using namespace cv;
+//using namespace Eigen;
 using namespace std;
 
 int main()
 {
-    std::ifstream file("D:/x/HF/GAMES101/HF/Assignment3/models/spotw/spot_triangulated_good.obj");
-    cout<<file.is_open();
+    cout << "hello world" << endl;
 }
