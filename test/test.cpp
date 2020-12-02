962e7bab721b81c6f3f0512e7a2f478a4f15409f
Sucong <sucongjs@outlook.com>
2020-12-02 15:48:51 +0800

    recover

--- test/test.cpp
+++ test/test.cpp
@@ -1,16 +1,14 @@
-//#include<opencv2/opencv.hpp>
-//#include<opencv2/highgui.hpp>
-//#include<eigen3/Eigen/Eigen>
-//#include<opencv2/opencv.hpp>
+#include<eigen3/Eigen/Eigen>
 #include<iostream>
-//#include <string>
-//#include <functional>
-//#include <optional>
-//using namespace cv;
-//using namespace Eigen;
+#include <string>
+#include <functional>
+#include <optional>
+using namespace cv;
+using namespace Eigen;
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-    cout << "hello world" << endl;
+    cout << __cplusplus << endl;
+    return 0;
 }
