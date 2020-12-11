5ab71e7bcb5a906023ed143444603cedda19e1a6
Sucong <sucongjs@outlook.com>
2020-12-11 13:50:05 +0800

    bazier curves--de casteljau's algorithm

--- Assignment4/main.cpp
+++ Assignment4/main.cpp
@@ -11,7 +11,7 @@ void mouse_handler(int event, int x, int y, int flags, void *userdata)
         std::cout << "Left button of the mouse is clicked - position (" << x << ", "
         << y << ")" << '\n';
         control_points.emplace_back(x, y);
-    }     
+    }
 }
 
 void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window) 
@@ -23,29 +23,59 @@ void naive_bezier(const std::vector<cv::Point2f> &points, cv::Mat &window)
 
     for (double t = 0.0; t <= 1.0; t += 0.001) 
     {
-        auto point = std::pow(1 - t, 3) * p_0 + 3 * t * std::pow(1 - t, 2) * p_1 +
-                 3 * std::pow(t, 2) * (1 - t) * p_2 + std::pow(t, 3) * p_3;
-
+        auto point = p_0 * std::pow(1 - t, 3) + 
+                     p_1 * 3 * t * std::pow(1 - t, 2) +
+                     p_2 * 3 * std::pow(t, 2) * (1 - t) + 
+                     p_3 * std::pow(t, 3);
         window.at<cv::Vec3b>(point.y, point.x)[2] = 255;
     }
 }
 
-cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, float t) 
+/*
+* 该函数使用一个控制点序列和一个浮点数 t 作为输入，实现 de Casteljau 算法来返回 Bézier 曲线上对应点的坐标。
+* */
+cv::Point2f recursive_bezier(std::vector<cv::Point2f> &control_points, float t, int size) 
 {
     // TODO: Implement de Casteljau's algorithm
-    return cv::Point2f();
+    // std::cout<<((control_points[1] - control_points[0]) * t);
+    cv::Point2f p = control_points[0] + (control_points[1] - control_points[0]) * t;
+    if(control_points.size() == 2)
+    {
+        return p;
+    }
 
+    control_points.push_back(p);
+    control_points.erase(control_points.begin());
+    
+    if(--size == 1)
+    {
+        control_points.erase(control_points.begin());
+        size = control_points.size();
+    }
+
+    return recursive_bezier(control_points, t, size);
 }
 
+/*
+* 该函数实现绘制 Bézier 曲线的功能。
+使用一个控制点序列和一个 OpenCV::Mat 对象作为输入，没有返回值。
+该函数使 t 在 0 到 1 的范围内进行迭代，并在每次迭代中使 t 增加一个微小值。对于每个需要计算的 t，将调用另一个函数 recursive_bezier，然后该函数将返回在 Bézier 曲线上 t 处的点。最后，将返回的点绘制在 OpenCV::Mat 对象上
+* */
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
     // recursive Bezier algorithm.
-
+    for (double t = 0.0; t <= 1.0; t += 0.001) 
+    {   
+        auto tmp_control_points = control_points;  // copy一份
+        auto point = recursive_bezier(tmp_control_points, t, control_points.size());
+        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+    }
 }
 
 int main() 
 {
+
     cv::Mat window = cv::Mat(700, 700, CV_8UC3, cv::Scalar(0));
     cv::cvtColor(window, window, cv::COLOR_BGR2RGB);
     cv::namedWindow("Bezier Curve", cv::WINDOW_AUTOSIZE);
@@ -62,8 +92,10 @@ int main()
 
         if (control_points.size() == 4) 
         {
-            naive_bezier(control_points, window);
-            //   bezier(control_points, window);
+            
+            std::cout<<(control_points[1] - control_points[0]) * 0.001<<'\n';
+            naive_bezier(control_points, window);  // red
+            bezier(control_points, window);  // green
 
             cv::imshow("Bezier Curve", window);
             cv::imwrite("my_bezier_curve.png", window);
@@ -76,5 +108,5 @@ int main()
         key = cv::waitKey(20);
     }
 
-return 0;
+    return 0;
 }
